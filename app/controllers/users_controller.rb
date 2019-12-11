7e6824149abf6c489e54eba4cb03a7a5a2fbe9e7
mizu19961203 <mizu19961203@gmail.com>
2019-12-11 11:34:05 +0000

    associate_post_usercontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+before_action :authenticate_user!
+before_action :ensure_correct_user,{only: [:edit, :update]}
   def show
   	@user = User.find(params[:id])
   	@posts = @user.posts
@@ -8,8 +10,21 @@ class UsersController < ApplicationController
   	@user = User.find(params[:id])
   end
 
+  def update
+  	@user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(current_user)
+  end
+
   private
     def user_params
     	params.require(:user).permit(:name)
+    end
+
+    def ensure_correct_user
+      @user = User.find(params[:id])
+      if current_user != @user
+        redirect_to user_path(current_user)
+      end
     end
 end
