7a4cd299a68e8a6daacdad2142f5871da4232498
mizu19961203 <mizu19961203@gmail.com>
2019-12-11 09:56:32 +0000

    created_post_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,45 @@
 class PostsController < ApplicationController
+	def new
+		@post = Post.new
+	end
+
+	def create
+		post = Post.new(blog_params)
+        post.save
+        redirect_to posts_path
+	end
+
+	def index
+		@posts = post.all
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def destroy
+		
+	end
+
+	def new
+		
+	end
+
+	 private
+    def post_params
+      params.require(:post).permit(:user_id,
+      							   :country_id,
+      							   :visa_id,
+      							   :vaccine_id,
+      							   :comment_id,
+      							   :image_id,
+      							   :movie,
+      							   :memo,
+      							   :route,)
+    end
+
 end
