f6e2e2f2a249633bbb8527f198a5cac70e1c02ae
mizu19961203 <mizu19961203@gmail.com>
2019-12-22 10:23:12 +0000

    fix puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -38,3 +38,13 @@ plugin :tmp_restart
 
 bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
 rails_root = Dir.pwd
+if Rails.env.production?
+    pidfile File.join(rails_root, 'tmp', 'pids', 'puma.pid')
+    state_path File.join(rails_root, 'tmp', 'pids', 'puma.state')
+    stdout_redirect(
+      File.join(rails_root, 'log', 'puma.log'),
+      File.join(rails_root, 'log', 'puma-error.log'),
+      true
+    )
+    daemonize
+end
