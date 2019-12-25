c437103127886c5e88cd81cecd522f59ba3aaa68
mizu19961203 <mizu19961203@gmail.com>
2019-12-25 05:16:33 +0000

    design about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-	root 'posts#index'
 	get 'home/about' => 'home#about'
+	root 'home#about'
 	devise_for :users
 	resources :posts do
 		resources :comments
-		resources :favorites, only: [:create, :destroy]
 	end
 	resources :users, only: [:show, :edit, :update, :destroy]
 end
+
 
