6d2b73a59d9a7cfa332c7682afd01588fd02bffb
mizu19961203 <mizu19961203@gmail.com>
2020-02-15 02:56:35 +0000

    replace db4

--- db/migrate/20200126061943_rename_admin_name_to_admins.rb
+++ db/migrate/20200126061943_rename_admin_name_to_admins.rb
@@ -1,6 +1,4 @@
 class RenameAdminNameToAdmins < ActiveRecord::Migration[5.2]
   def change
-  	rename_column :admins, :admin_email, :email
-  	rename_column :admins, :admin_password, :password
   end
 end
