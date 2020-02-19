aae498ea7a5465e36833d200d199d97156c4ab19
mizu19961203 <mizu19961203@gmail.com>
2020-02-19 09:07:58 +0000

    replace db5

--- db/migrate/20191211064931_create_admins.rb
+++ db/migrate/20191211064931_create_admins.rb
@@ -5,7 +5,7 @@ class CreateAdmins < ActiveRecord::Migration[5.2]
     	t.string :admin_email, null: false
     	t.string :admin_password, null: false
     	t.datetime :created_at, null: false, default:'now'
-        t.datetime :updated_at, null: false, default:'now'
+      t.datetime :updated_at, null: false, default:'now'
 
 
       t.timestamps
